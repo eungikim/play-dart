ad07f038e344595742360f0e5b43c1d6c2e0bb95
eungikimme <eungikim.me@gmail.com>
2019-10-22 23:42:03 +0900

    Write language sample 1/2

--- play.dart
+++ play.dart
@@ -1,8 +1,56 @@
 
 void main() {
   print("Hello World");
+
+  Play();
 }
 
-class Play {
-  
+void Play() {
+  // Variables
+  var name = 'Voyager I';
+  var year = 1977;
+  var antennaDiameter = 3.7;
+  var flybyObject = ['Jupiter', 'Seturn', 'Uranus', 'Neptune'];
+  var image = {
+    'tags' : ['saturn'],
+    'url' : '//path/to/saturn.jpg'
+  };
+
+  // Control Flow Statements
+  if (year >= 2001) {
+    print('21st century');
+  } else if (year >= 1901) {
+    print('20th century');
+  }
+
+  for (var object in flybyObject) {
+    print(object);
+  }
+
+  for (int month = 1; month <= 12; month++) {
+    print(month);
+  }
+
+  while (year < 2016) {
+    year += 1;
+  }
+
+  // Functions
+  print(fibonacci(12));
+  flybyObject.where((name) => name.contains('tu')).forEach(print);
+
+  // Comments
+  // This is a normal, one-line comment.
+
+  /// This is a documentation comment, used to document libraries,
+  /// classes, and their members. Tools like IDEs and dartdoc treat
+  /// doc comments specially.
+
+  /* Comments like these are also supported. */
+}
+
+// Functions
+int fibonacci(int n) {
+  if (n == 0 || n == 1) return n;
+  return fibonacci(n - 1) + fibonacci(n - 2);
 }
