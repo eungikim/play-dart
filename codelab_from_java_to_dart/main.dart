7a09fcd51dfb326240a57eea9f941ea411ca7f04
eungikimme <eungikim.me@gmail.com>
2019-10-26 02:12:23 +0900

    Write codelab page 3

---
+++ codelab_from_java_to_dart/main.dart
@@ -0,0 +1,13 @@
+class Bicycle {
+  // https://gist.github.com/eungikimme/4841374162ebf275181df5ed59a43cae
+  // https://dartpad.dartlang.org/4841374162ebf275181df5ed59a43cae
+}
+
+class Rectangle {
+  // https://gist.github.com/eungikimme/393484d725f9aa2025ea256b1362e4f2
+  // https://dartpad.dartlang.org/393484d725f9aa2025ea256b1362e4f2
+
+}
+
+void main() {
+}
