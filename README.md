92fcc2de3a29ab6ecec01349cff92f93fd7acc59
eungikimme <eungikim.me@gmail.com>
2019-11-02 01:12:13 +0900

    Edit doc

--- README.md
+++ README.md
@@ -6,3 +6,6 @@ Practice for dart language
 
 ## language_samples
 [First page of Dart document](https://dart.dev/samples)
+
+## codelab_from_java_to_dart
+[Java-to-Dart codelab](https://codelabs.developers.google.com/codelabs/from-java-to-dart)
--- codelab_from_java_to_dart/main.dart
+++ codelab_from_java_to_dart/main.dart
@@ -1,12 +1,18 @@
 class Bicycle {
+  // #1
   // https://gist.github.com/eungikimme/4841374162ebf275181df5ed59a43cae
   // https://dartpad.dartlang.org/4841374162ebf275181df5ed59a43cae
 }
 
 class Rectangle {
+  // #2
   // https://gist.github.com/eungikimme/393484d725f9aa2025ea256b1362e4f2
   // https://dartpad.dartlang.org/393484d725f9aa2025ea256b1362e4f2
 
+}
+
+class Shape {
+  // #3
 }
 
 void main() {
